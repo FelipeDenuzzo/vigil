0caf6e82dda6caebeca2f69aa80953d90fb50628
FelipeDenuzzo <felipedenuzzo@gmail.com>
2026-06-01 18:33:47 -0300

    Alteração no avaliador 2

---
+++ src/attentions/selective/games/VisualSearchHunt/assessment-v2/index.ts
@@ -0,0 +1,5 @@
+/* assessment-v2/index.ts */
+/* Mapeamento de re-exportações para compatibilidade com useVisualSearchEvaluation.ts */
+
+export { runVisualSearchV2 as buildVisualSearchV2Result } from "./runVisualSearchV2";
+export type { VisualSearchV2Result as VisualSearchV2AssessmentResult } from "./visualSearchV2.types";
