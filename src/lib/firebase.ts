cc10fc66e257a1f4ebd5c12d9fc01d80368fa5ef
Felipe Denuzzo <152036119+FelipeDenuzzo@users.noreply.github.com>
2026-06-10 20:48:48 -0300

    feat: add Firebase Storage instance to firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,6 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
   apiKey: "[REDACTED_FIREBASE_API_KEY]",
@@ -13,5 +14,6 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const storage = getStorage(app);
 const db = getFirestore(app);
 export default db;
