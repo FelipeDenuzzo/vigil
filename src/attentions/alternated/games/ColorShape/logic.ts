72dbc1c3caabde7e3acbac9c7ba568c74f87251b
Felipe Denuzzo <152036119+FelipeDenuzzo@users.noreply.github.com>
2026-06-20 01:09:20 -0300

    fix(ColorShape): remove shuffle não utilizado (TS6133)

--- src/attentions/alternated/games/ColorShape/logic.ts
+++ src/attentions/alternated/games/ColorShape/logic.ts
@@ -7,14 +7,6 @@ function randInt(min: number, max: number): number {
 function pick<T>(arr: T[]): T {
   return arr[Math.floor(Math.random() * arr.length)];
 }
-function shuffle<T>(arr: T[]): T[] {
-  const a = [...arr];
-  for (let i = a.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [a[i], a[j]] = [a[j], a[i]];
-  }
-  return a;
-}
 
 export function generateRuleSequence(totalTrials: number): RuleType[] {
   const seq: RuleType[] = [];
